a3284cf26197bd64e944907d59a7e8ad8a21dee3
Mitchell Andrews <rastaninja77@gmail.com>
2018-05-05 17:08:53 -0400

    Updating Prefabs

---
+++ Assets/Scripts/SharkController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkController : PlayerController
+{
+    private enum CharacterState
+    {
+        Idle,
+        Moving,
+        PrimaryAttack,
+        SecondaryAttack,
+        Recovery
+    }
+
+    private CharacterState m_state;
+    private CharacterState m_previousState;
+
+    public Animator SkinAnimator;
+
+    private void Awake()
+    {
+        OnMovementStep += OnMove;
+        SkinAnimator.Play("Idle");
+        m_state = CharacterState.Idle;
+    }
+
+    private void OnMove(float val)
+    {
+        if(m_state != CharacterState.Moving)
+        {
+            SkinAnimator.Play("Walk");
+            m_state = CharacterState.Moving;
+        }
+    }
+
+    private void PrimaryAttack()
+    {
+        m_state = CharacterState.PrimaryAttack;
+        SkinAnimator.Play("Slam");
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            PrimaryAttack();
+        }
+
+        if (m_state != m_previousState)
+        {
+
+        }
+    }
+
+}
