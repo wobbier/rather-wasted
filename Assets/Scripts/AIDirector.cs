0fe2b2f9b5da7c051b50148fe66d095ce8ed2b29
Mitchell Andrews <rastaninja77@gmail.com>
2018-05-05 00:21:07 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/AIDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDirector : MonoBehaviour {
+
+    public int InitialAmountToSpawn = 5;
+
+    public GameObject Civillian;
+
+    private AISpawnPoint[] m_spawnPoints;
+
+    // Use this for initialization
+    void Awake()
+    {
+        m_spawnPoints = FindObjectsOfType<AISpawnPoint>();
+
+        for (int i = 0; i < InitialAmountToSpawn; ++i)
+        {
+            Instantiate(Civillian, m_spawnPoints[Random.Range(0, m_spawnPoints.Length)].transform.position, Quaternion.identity);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CivillianAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(SphereCollider))]
+public class CivillianAI : MonoBehaviour
+{
+    private NavMeshAgent m_nav;
+    // Use this for initialization
+    void Start()
+    {
+        m_nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!m_nav.hasPath)
+        {
+            m_nav.SetDestination(new Vector3(Random.Range(0, 20), 0, Random.Range(0, 20)));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+
+        }
+    }
+}
