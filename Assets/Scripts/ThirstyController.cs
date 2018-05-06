5697b882c8a4f8870d1c55e9b853bbc8c42c1a20
Mitchell Andrews <rastaninja77@gmail.com>
2018-05-06 05:02:50 -0400

    Shark animations

--- Assets/Scripts/ThirstyController.cs
+++ Assets/Scripts/ThirstyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SharkController : PlayerController
+public class ThirstyController : PlayerController
 {
     private enum CharacterState
     {
